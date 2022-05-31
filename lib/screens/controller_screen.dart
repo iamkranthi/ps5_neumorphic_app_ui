64726d7189e10a168ee45f2147f1eba7d3edb1d6
Kranthi <kranthigowthamkumar@gmail.com>
2022-06-01 03:20:24 +0530

    controller page completed

--- lib/screens/controller_screen.dart
+++ lib/screens/controller_screen.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 import '../imports.dart';
 
 class ControllerScreen extends StatelessWidget {
@@ -13,7 +13,7 @@ class ControllerScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Background(height: height * 0.5, width: width),
+          Background(height: height * 0.47, width: width),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -25,8 +25,8 @@ class ControllerScreen extends StatelessWidget {
               ),
               Container(
                 height: 15,
-                width: 470,
-                color: Colors.yellow,
+                width: 270,
+                color: ConstantColors.cardcolor,
               ),
               const TitleText(
                 titletext: 'Dual',
@@ -34,20 +34,89 @@ class ControllerScreen extends StatelessWidget {
                 choosecolor: Colors.white,
               ),
               const SizedBox(
-                height: 50,
+                height: 7,
               ),
               Card(
+                elevation: 20,
+                shadowColor: ConstantColors.blackcolor,
                 color: Colors.amberAccent,
                 margin: const EdgeInsets.all(10),
-                shape: const StadiumBorder(
-                  side: BorderSide(
-                    width: 4,
-                    style: BorderStyle.solid,
-                  ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(70),
                 ),
                 child: Image.asset(
                   'assets/images/card_images/ps5.png',
                   width: 400,
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    CupertinoIcons.arrowtriangle_up_circle_fill,
+                    size: 50,
+                  ),
+                  Text(
+                    'Haptic feedback    ',
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Icon(
+                    CupertinoIcons.smallcircle_fill_circle_fill,
+                    size: 50,
+                  ),
+                  Text(
+                    'Futuristic design    ',
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 29,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    CupertinoIcons.stop_circle_fill,
+                    size: 50,
+                  ),
+                  Text(
+                    'Type C-fast charging ',
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                      fontSize: 14,
+                    ),
+                  ),
+                  Icon(
+                    CupertinoIcons.xmark_circle_fill,
+                    size: 50,
+                  ),
+                  Text(
+                    'Built in microphone',
+                    style: TextStyle(
+                      fontFamily: 'Nunito',
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(13.0),
+                child: Text(
+                  'Discover a deeper, highly immersive gaming experience with the innovative new PS5™ controller, featuring haptic feedback and dynamic trigger effects2. The DualSense wireless controller also includes a built-in microphone and create button, all integrated into an iconic, comfortable design.',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      wordSpacing: 2),
                 ),
               )
             ],
